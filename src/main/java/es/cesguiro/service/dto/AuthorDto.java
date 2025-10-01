ee9c0f8e990e8f6af2580357c777cfac6615a157
luismi <olmedaromeroluismiguel@gmail.com>
2025-10-01 12:48:54 +0200

    added validation to authorDto and publisherDto

--- src/main/java/es/cesguiro/service/dto/AuthorDto.java
+++ src/main/java/es/cesguiro/service/dto/AuthorDto.java
@@ -1,13 +1,26 @@
 package es.cesguiro.service.dto;
 
+import jakarta.validation.constraints.*;
+
 public record AuthorDto(
         Long id,
+
+        @NotBlank(message = "El nombre no puede estar vacío")
         String name,
+
+        @NotBlank(message = "La nacionalidad no puede estar vacía")
         String nationality,
         String biographyEs,
         String biographyEn,
+
+        @Min(value = 0, message = "El año de nacimiento debe ser positivo")
         int birthYear,
+
+        @Min(value = 0, message = "El año de defunción debe ser positivo")
         Integer deathYear,
+
+        @NotBlank(message = "El slug no puede estar vacío")
+        @Pattern(regexp = "^[a-z0-9-]+$", message = "El slug solo puede contener minúsculas, números y guiones")
         String slug
 ) {
 }
--- src/main/java/es/cesguiro/service/dto/PublisherDto.java
+++ src/main/java/es/cesguiro/service/dto/PublisherDto.java
@@ -1,8 +1,16 @@
 package es.cesguiro.service.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record PublisherDto(
         Long id,
+
+        @NotBlank(message = "El nombre no puede estar vacío")
         String name,
+
+        @NotBlank(message = "El slug no puede estar vacío")
+        @Pattern(regexp = "^[a-z0-9-]+$", message = "El slug solo puede contener minúsculas, números y guiones")
         String slug
 ) {
 }
