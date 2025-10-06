05c8a6aafdc3d6d525d7893d9959392268cb9517
Ismael Monleón Rubio <ismael.monleon73@gmail.com>
2025-10-06 10:14:20 +0200

    Fix conflicts

--- src/main/java/es/cesguiro/mapper/BookMapper.java
+++ src/main/java/es/cesguiro/mapper/BookMapper.java
@@ -49,11 +49,7 @@ public class BookMapper {
 
     public BookEntity fromBookToBookEntity(Book book) {
         if (book == null) {
-<<<<<<< HEAD
             throw new BusinessException("Book cannot be null");
-=======
-            throw  new BusinessException("Book is null");
->>>>>>> aa8b326e3246ebe172c3d265dfe131910d8f1754
         }
         return new BookEntity(
                 book.getId(),
